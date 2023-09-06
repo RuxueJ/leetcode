0d6344bb4bfa70e870ed5272dc33ca626f17883c
RuxueJ <rjin@sfsu.edu>
2023-09-05 19:45:49 -0700

    9/5

---
+++ Java/src/ValidSudoku/Solution.java
@@ -0,0 +1,26 @@
+package ValidSudoku;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set seen = new HashSet();
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char value = board[i][j];
+                if(value != '.'){
+                    if(!seen.add(value + "in row" + i) ||
+                            !seen.add(value + "in column" + j) ||
+                            !seen.add(value + "in block" + i/3 + "-" + j/3)){
+                        return false;
+                    }
+                }
+
+            }
+
+        }
+        return true;
+
+    }
+}
